20e616160dcb4cfafd141b19f4875a16f9fa9a98
jihye <asd36384@gmail.com>
2021-11-04 23:03:16 +0900

    sc

--- src/_actions/user_action.js
+++ src/_actions/user_action.js
@@ -5,7 +5,7 @@ function auth() {
   //최초로 로그인 시도한 경오
   //로그인 안 한 유저가 접속 한 경우
   const request = instance
-    .get("/api/auth/user", localStorage.getItem("token"))
+    .get("/api/user", localStorage.getItem("token"))
     .then((response) => response.data);
 
   return {
