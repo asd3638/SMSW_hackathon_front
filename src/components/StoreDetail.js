2ef086cfa6c40b8f641987abba219d0c0c6ffe52
jihye <asd36384@gmail.com>
2021-11-07 00:20:06 +0900

    merge

--- src/components/StoreDetail.js
+++ src/components/StoreDetail.js
@@ -3,28 +3,37 @@ import instance from "../lib/api/instance";
 import { Card } from "react-bootstrap";
 
 function StoreDetail({ userSeq, storeSeq }) {
-  const [storeDetail, setStoreDetail] = useState();
+  const [couponList, setCouponList] = useState([]);
+  const [store, setStore] = useState();
 
   useEffect(() => {
     const fetchStore = async () => {
       try {
         const response = await instance.get(`/api/map/${userSeq}/${storeSeq}`);
-        console.log(response);
-        setStoreDetail(response.data); // 데이터는 response.data 안에 들어있습니다.
+        setStore(response.data.store); // 데이터는 response.data 안에 들어있습니다.
+      } catch (e) {}
+    };
+    const fetchCouponList = async () => {
+      try {
+        const response = await instance.get(`/api/map/${userSeq}/${storeSeq}`);
+        setCouponList(response.data.coupon); // 데이터는 response.data 안에 들어있습니다.
       } catch (e) {}
     };
     fetchStore();
+    fetchCouponList();
   }, []);
   return (
-    <Card>
-      <Card.Header as="h5">
-        {storeDetail ? storeDetail.store.name : null}
-      </Card.Header>
-      <Card.Body>
-        <Card.Title>symbol</Card.Title>
-        <Card.Text>가게 정보</Card.Text>
-      </Card.Body>
-    </Card>
+    <div>
+      {store ? (
+        <Card>
+          <Card.Header as="h5">{store.name}</Card.Header>
+          <Card.Body>
+            <Card.Title>symbol</Card.Title>
+            <Card.Text>가게 정보</Card.Text>
+          </Card.Body>
+        </Card>
+      ) : null}
+    </div>
   );
 }
 
