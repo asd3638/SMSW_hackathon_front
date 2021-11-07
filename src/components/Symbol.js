67f1c352350ee52e5cb4060aa69008d98f58a3da
jihye <asd36384@gmail.com>
2021-11-07 13:36:30 +0900

    FIX(storepage): fix store page

---
+++ src/components/Symbol.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+function Symbol() {
+  return (
+    <Container>
+      <Row>
+        <Col style={{ width: "20px" }}>a</Col>
+        <Col>bbbbbbbbbbbbbb</Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Symbol;
--- src/views/StorePage.js
+++ src/views/StorePage.js
@@ -1,7 +1,53 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import instance from "../lib/api/instance";
+import StoreDetail from "../components/StoreDetail";
+import Symbol from "../components/Symbol";
 function StorePage() {
-  return <div>This is store page</div>;
+  const user = useSelector((state) => state.user.userData);
+  const [storeList, setStoreList] = useState([]);
+  const [symbolList, setSymbolList] = useState([]);
+  const [currentUser, setCurrentUser] = useState({});
+  useEffect(() => {
+    const load = async () => {
+      try {
+        if (user !== undefined) {
+          setCurrentUser(user);
+        }
+      } catch (error) {}
+    };
+    const fetchStoreList = async () => {
+      try {
+        const response = await instance
+          .get(`/api/store/${currentUser.id}`)
+          .then((response) => {
+            return response.data.store;
+          });
+        setStoreList(response); // 데이터는 response.data 안에 들어있습니다.
+      } catch (e) {}
+    };
+    const fetchSymbolList = async () => {
+      try {
+        const response = await instance.get(`/api/symbol`).then((response) => {
+          return response.data;
+        });
+        setSymbolList(response); // 데이터는 response.data 안에 들어있습니다.
+      } catch (e) {}
+    };
+    fetchStoreList();
+    fetchSymbolList();
+    load();
+  }, [user]);
+
+  return (
+    <Container>
+      <Symbol />
+      {storeList.map((store, idx) => (
+        <StoreDetail selectedStore={store} />
+      ))}
+    </Container>
+  );
 }
 
 export default StorePage;
